0828bb53fd121d3cb024de93ee79230107c120f3
Codeword <saldatejared@gmail.com>
2022-07-17 13:57:25 -0700

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,13 @@
 //Jared 
 
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 50;
@@ -26,14 +34,16 @@ var fight = function(enemyName) {
     if (confirmSkip) {
       window.alert(playerName + ' has decided to skip this fight. Goodbye!');
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 10;
+      playerMoney = Math.max(0, playerMoney - 10);
       console.log("playerMoney", playerMoney)
       break;
     }
   }
 
-    // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+  // generate random damage value based on player's attack power
+var damage = randomNumber(playerAttack - 3, playerAttack);
+
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -52,7 +62,10 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+  var damage = randomNumber(enemyAttack - 3, enemyAttack);
+
+  playerHealth = Math.max(0, playerHealth - damage);
+
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -85,7 +98,7 @@ var startGame = function(){
       var pickedEnemyName = enemyNames[i];
   
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
   
       // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -95,7 +108,7 @@ var startGame = function(){
           // ask if player wants to use the store before next round
   var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
 
-  // if yes, take them to the store() function
+     // if yes, take them to the store() function
   if (storeConfirm){ 
         shop();
         }
@@ -180,3 +193,4 @@ switch (shopOptionPrompt) {
 };
 // start first game when page loads
 startGame();
+
